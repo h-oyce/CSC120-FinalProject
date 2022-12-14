a5c2a87bfe48910528d2fd3e30838d36bd135767
Juniper Huang <93298822+h-oyce@users.noreply.github.com>
2022-12-14 12:17:21 -0500

    Create Images.java

---
+++ Images.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.awt.*;
+import java.lang.Object;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class Images {
+    
+}
