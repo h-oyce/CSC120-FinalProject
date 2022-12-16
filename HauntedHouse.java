e5c4f91a8a378186990018ddf9643e5613c6b33c
lzguo25 <113069414+lzguo25@users.noreply.github.com>
2022-12-16 13:48:29 -0500

    Update HauntedHouse.java

--- HauntedHouse.java
+++ HauntedHouse.java
@@ -1,10 +1,17 @@
+/*
+ * HauntedHouse class
+ * Contains the attributes and constructor for the HauntedHouse, which the Player will enter to start the game.
+ * @param String name, String address, int numRooms, String description
+ * @return Prints out the instance of HauntedHouse, which will be the house Players explore once they enter.
+ */
+ 
 public class HauntedHouse{
     private String address;
     private String name;
     private int numRooms;
     private String description;
 
-
+    /*Constructor */
     public HauntedHouse(String name, String address, int numRooms, String description){
         this.name = name;
         this.address= address;
@@ -12,6 +19,7 @@ public class HauntedHouse{
         this.description = description;
     }
 
+    /*Accessors */
     public String getName(){
         return this.name;
     }
@@ -28,8 +36,7 @@ public class HauntedHouse{
         return this.description;
     }
 
-    /*Navigation Methods? */
-
+    /*Enter Method -- starts the game*/
     public HauntedHouse enter(){
         System.out.println("You've entered " + this.name +".");
         return this;
@@ -40,12 +47,10 @@ public class HauntedHouse{
     
     // }
 
-    public String toString(){
+    public String toString(){//prints out the information of the HauntedHouse instance.
         return (this.name + " is a haunted house located at " + this.address + "." + this.description);
     }
     public static void main(String args[]){
-        
-
 
     }
 }
